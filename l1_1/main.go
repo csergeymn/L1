d2d52f77f4f6d47e89e94374bfe1e52955ae925e
s.kopylov <s.kopylov@notamedia.ru>
2025-08-31 22:18:24 +0300

    L1.1 - embedded struct

---
+++ l1_1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	Age    uint8
+	Name   string
+	Gender string
+}
+
+func (h Human) ShowName() {
+	fmt.Printf("Hello, my name is %s\n", h.Name)
+}
+
+func (h Human) ShowStats() {
+	fmt.Printf("Hello, i am %s %s %d years old\n", h.Gender, h.Name, h.Age)
+}
+
+type Action struct {
+	Human
+	Action string
+}
+
+func (a Action) ShowStats() {
+	fmt.Printf("Hello, i am %s %s %d years old with action '%s'\n", a.Gender, a.Name, a.Age, a.Action)
+}
+
+func (a Action) ShowAction() {
+	fmt.Printf("Hello, i am %s and i can %s\n", a.Name, a.Action)
+}
+
+func main() {
+	Alice := Human{
+		Age:    18,
+		Name:   "Alice",
+		Gender: "Female",
+	}
+
+	Alice.ShowName()
+	Alice.ShowStats()
+
+	MoveForward := Action{
+		Human:  Alice,
+		Action: "Move Forward",
+	}
+
+	MoveForward.ShowName()
+	MoveForward.ShowStats()
+	MoveForward.ShowAction()
+}
