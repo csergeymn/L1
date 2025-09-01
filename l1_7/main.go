b52ce1d9cd47efa2b50fd2dde6a2bc2f4182b9d4
s.kopylov <s.kopylov@notamedia.ru>
2025-09-02 00:42:50 +0300

    L1.7 - concurrent map write

---
+++ l1_7/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	concurrentMap   = make(map[string]int)
+	concurrentMapMu sync.Mutex
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	// Запускаем 10 горутин, каждая пишет в карту
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			fmt.Printf("Goroutine %d started\r\n", i)
+			defer wg.Done()
+			key := fmt.Sprintf("key%d", i)
+			concurrentMapMu.Lock()
+			defer concurrentMapMu.Unlock()
+			concurrentMap[key] = i
+		}(i)
+	}
+
+	wg.Wait()
+
+	// Читаем результат
+	fmt.Println("Result:", concurrentMap)
+	for i := 0; i < 10; i++ {
+		key := fmt.Sprintf("key%d", i)
+		fmt.Println(key, concurrentMap[key])
+	}
+}
