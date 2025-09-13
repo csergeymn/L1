cf7c3fc2932650105f7e60af6e3b37be7bcea17a
s.kopylov <s.kopylov@notamedia.ru>
2025-09-14 00:48:21 +0300

    L1.13 - type detection

---
+++ l1_14/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// универсальная функция определения типа
+func detectType(v interface{}) {
+	switch v.(type) {
+	case int:
+		fmt.Printf("Type: int, Value: %v\n", v)
+	case string:
+		fmt.Printf("Type: string, Value: %v\n", v)
+	case bool:
+		fmt.Printf("Type: bool, Value: %v\n", v)
+	case chan any:
+		fmt.Printf("Type: chan, Value: %v\n", v)
+	default:
+		fmt.Printf("Unknown type, Value: %v\n", v)
+	}
+}
+
+func main() {
+	detectType(42)      // int
+	detectType("hello") // string
+	detectType(true)    // bool
+
+	chInt := make(chan int)
+	detectType(chInt) // chan
+
+	chStr := make(chan string)
+	detectType(chStr) // chan
+
+	detectType(3.14) // unknown - no case branch
+}
